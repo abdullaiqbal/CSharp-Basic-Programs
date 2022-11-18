1a81afdd48a8dbe68ba76b130c1ab91fb762362e
Abdullah Iqbal <101557230+abdullaiqbal@users.noreply.github.com>
2022-11-18 11:29:59 +0500

    Add project files.

---
+++ precticeCSharp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+class Demo
+{
+
+    public static void Main(String[] args)
+    {
+
+
+
+
+        // Randomly Swap without using any third variable
+        //string str = "hello world";
+        //str = new string(str.ToCharArray().
+        //                 OrderBy(s => (new Random().Next(2) % 2) == 0).ToArray());
+        //foreach (var value in str)
+        //{
+        //    Console.WriteLine(value);
+        //}
+
+        //Swaping Two Array without using Third Variable
+        //String[] arr1 = {"Abdullah","Ali","Arslan"};
+        //String[] arr2 = {"Zeeshan","Enam" };
+
+        //Console.WriteLine($"Array Befaor Swap \n");
+        //Console.WriteLine("First Array\n");
+        //foreach (var value in arr1)
+        //{
+        //    Console.WriteLine(value);
+        //}
+        //Console.WriteLine("\nSecond Array\n");
+        //foreach (var value in arr2)
+        //{
+        //    Console.WriteLine(value);
+        //}
+        //arr1 = arr1.Concat(arr2).ToArray();
+        //Console.WriteLine($"\nConcatinated Array\n");
+        //foreach (var value in arr1)
+        //{
+        //    Console.WriteLine(value);
+        //}
+        //Console.WriteLine($"\nAfter Swapping\n");
+        //Console.WriteLine($"\nArray 2\n");
+        //arr2 = arr1.Take(arr1.Length-arr2.Length).ToArray();
+        //foreach (var value in arr2)
+        //{
+        //    Console.WriteLine(value);
+        //}
+
+        //Console.WriteLine($"\nArray 1\n");
+        //arr1 = new List<string>(arr1).GetRange(arr1.Length-arr2.Length, arr1.Length - arr2.Length).ToArray();
+
+        //foreach (var value in arr1)
+        //{
+        //    Console.WriteLine(value);
+        //}
+
+
+        //Swaping Two String without using Third Variable
+
+        //arr1 = arr1.
+        //arr1 = arr1.TakeLast<String[]>(arr1.Length - arr2.Length);
+        //arr2 = arr2.CopyTo(arr1, Convert.ToInt32(arr1.Length - arr2.Length));
+
+        //String str1 = "Brad";
+        //String str2 = "Pitt";
+
+        //Console.WriteLine("Strings before swap");
+        //Console.WriteLine(str1);
+        //Console.WriteLine(str2);
+
+        //str1 = str1 + str2;
+
+        //str2 = str1.Substring(0, str1.Length - str2.Length);
+        //str1 = str1.Substring(str2.Length);
+
+        //Console.WriteLine("Strings after swap");
+        //Console.WriteLine(str1);
+        //Console.WriteLine(str2);
+
+
+
+
+        //public static string Scramble(this string s)
+        //{
+        //    return new string(s.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
+        //}
+    }
+}
+
